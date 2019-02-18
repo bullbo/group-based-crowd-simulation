eff775a7c11a52b8036b51a8bd56c5ff0cc2fcdb
bullbo <amin.bullarif@gmail.com>
2019-02-18 14:47:00 +0100

    Add files via upload
    Added group attraction force so that the agents don't wander off alone without the group.

---
+++ v.1.3/Utilities.py
@@ -0,0 +1,84 @@
+import numpy as np
+import random
+import math
+from Constants import *
+
+
+def normalize(v):
+    """ Returns the normalized vector v."""
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+def random_circle(input_radius, scale):
+    """ Returns two points on separate sides of a dounut. """
+    theta = random.random()*2*(math.pi)
+
+    radius_start = random.uniform(input_radius, input_radius*scale)
+    radius_end = random.uniform(input_radius, input_radius*scale)
+
+    start_position = np.array(
+        [radius_start*math.cos(theta), radius_start*math.sin(theta)])
+    end_position = np.array(
+        [radius_end*math.cos(theta+math.pi), radius_end*math.sin(theta+math.pi)])
+
+    return [start_position, end_position]
+
+
+def calc_b(relative_distance, relative_velocity, dt):
+    """ Returns value for b, from Dirk Helbing paper.  """
+
+    norm_relative_distance = np.linalg.norm(relative_distance)
+    norm_relative_velocity = np.linalg.norm(relative_velocity)
+
+    temp_1 = (norm_relative_distance +
+              np.linalg.norm(relative_distance-relative_velocity*dt))
+    temp_2 = (dt*norm_relative_velocity)
+
+    return 0.5*np.sqrt(temp_1**2-temp_2**2)
+
+
+def calc_weight(l, angle):
+    """ Returns the weights for the social force based on the agents field of vision."""
+    return (l + (l-1)*(1-np.cos(angle))*0.5)
+
+
+def calc_angle(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'."""
+    #cosang = np.dot(v1, v2)
+    #sinang = np.linalg.norm(np.cross(v1, v2))
+    #return np.arctan2(sinang, cosang)
+    return v1[0]*v2[1] - v1[1]*v2[0]
+
+def rot_v(v1, theta):
+    """ Rotates a vector v1 theta degrees, returns the new rotated vector. """
+    theta = np.radians(theta)
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.array([[c, s], [-s, c]])
+    return (R.dot(v1))
+
+def angle2(v1, v2):
+    """ Returns the signed angle in radians between two vectors v1, v2. """
+    angle = np.arctan2(v2[1],v2[0]) - np.arctan2(v1[1],v1[0])
+    return angle
+
+def check_vector_inbetween(v1, v2, point):
+    """ Checks if point lies inbetween two vectors v1, v2. Returns boolean. """
+    if (np.dot(np.cross(v1, point), np.cross(v1, v2))) >=0 and (np.dot(np.cross(v2, point), np.cross(v2, v1))) >=0:
+        return True
+    else:
+        return False
+
+
+# NOT USED
+def turn_vector_to_point(v1, point, gaze_angle):
+    """ Turns a vector v1, so that its field of view defined by the gaze_angle includes a point. """
+    fov1, fov2 = rot_v(v1, -gaze_angle), rot_v(v1, gaze_angle)
+    if check_vector_inbetween(fov1, fov2, point) == False:
+        theta = -min(angle2(fov1, point), angle2(fov2, point), key=abs)
+        output = rot_v(v1, np.degrees(theta/2))
+    else:
+        output = v1
+    return output
